9795813e37aa7966153824bf5c8a9965ac140652
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-11-07 10:21:22 -0800

    Started to work on the business model

---
+++ business-models.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.write('Hello, business models!')
+
+st.sidebar.slider('A number', 0, 101)
+
+
+# User Growth Model
+#
+# marketing -> users
+# users -> users (virality)
+#
+# Timing Model
+#
+# When do we start major marketing?
+# When do we start getting paying customers?
+#
+# Payment Model
+#
+# users -> superusers
+# superusers -> paying customers
+# 
